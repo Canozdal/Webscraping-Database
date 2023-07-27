c916a05fc418e225270cdf2ff12cee87ece12a56
Can Ozdal <canozdal@hotmail.com>
2023-07-27 16:01:54 +0300

    Added cheap flight finding functionality. Need an API

--- Program.cs
+++ Program.cs
@@ -6,12 +6,14 @@ using System.Linq;
 using HtmlAgilityPack;
 using System.Collections;
 using MySql.Data.MySqlClient;
+using System.Net.Http;
+using System.Threading.Tasks;
 class Program
 {
-    static void Main()
+    static async Task Main()
     {
         string connection_string = "server=localhost;port=3306;database=news;user=root;password=password;";
-        ArrayList items = GetNews();
+        ArrayList items = GetNews("https://www.sondakika.com/", "//*[@id=\"bx-pager\"]");
         using (var connection = new MySqlConnection(connection_string))
         {
             connection.Open();
@@ -30,6 +32,7 @@ class Program
 
 
         Console.WriteLine(items.Count);
+
     }
     static string HandleTurkishCharacters(string text)
     {
@@ -38,12 +41,11 @@ class Program
 
         return Encoding.UTF8.GetString(bytes); 
     }
-    static ArrayList GetNews()
+    static ArrayList GetNews(string url,string xpath)
     {
         var web = new HtmlWeb();
-        var url = "https://www.sondakika.com/";
         var doc = web.Load(url);
-        var nodes = doc.DocumentNode.SelectNodes("//*[@id=\"bx-pager\"]");
+        var nodes = doc.DocumentNode.SelectNodes(xpath);
         ArrayList news = new ArrayList();
         if (nodes != null)
         {
@@ -59,5 +61,35 @@ class Program
             }
         }
         return news;
+    }
+
+    static async Task GetCheapestFlights(string apiKey,string origin, string destination,DateTime departureDate)
+    {
+        string base_url = "https://partners.api.skyscanner.net/apiservices/v3/flights/live/search/create";
+        string formattedDate = departureDate.ToString("yyyy-MM-dd");
+
+        string url = $"{base_url}?x-api-key={apiKey}&country=DE&currency=EUR&locale=en-US&originPlace={origin}&destinationPlace={destination}&outboundDate={formattedDate}&adults=1";
+        using (var httpClient = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(responseBody);
+                }
+                else
+                {
+                    Console.WriteLine($"Error code {response.StatusCode} : {response.ReasonPhrase}");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
     }
 }
